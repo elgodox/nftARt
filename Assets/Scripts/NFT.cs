e69fae67929f36dfdfc773a311fd12b55f6d9eb1
Alan Schumann <elgodox@gmail.com>
2022-01-14 10:37:28 -0300

    Resize images in correct format

--- Assets/Scripts/NFT.cs
+++ Assets/Scripts/NFT.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,12 +11,28 @@ public class NFT : MonoBehaviour, INFT
     {
         RawImage image = GetComponent<RawImage>();
         RectTransform rectTransform = GetComponent<RectTransform>();
-        rectTransform.sizeDelta = new Vector2(Screen.width, Screen.width);
 
         name = nft.id.ToString();
         image.texture = nft.texture;
-        
+
+        Vector2 size = ResizeImage(image);
+        rectTransform.sizeDelta = size;
+
         Instantiate(this, position);
+    }
+
+    private Vector2 ResizeImage(RawImage image)
+    {
+        var ratioX = (double)Screen.width / image.mainTexture.width;
+        var ratioY = (double)Screen.width / image.mainTexture.height;
+        var ratio = Math.Min(ratioX, ratioY);
+
+        var newWidth = (int)(image.mainTexture.width * ratio);
+        var newHeight = (int)(image.mainTexture.height * ratio);
+
+        var newSize = new Vector2(newWidth, newHeight);
+
+        return newSize;
     }
 
 }
